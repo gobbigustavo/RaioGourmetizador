7a2c768d0cd825bd0fc3498f6cb653b4b396651d
Filipe Falcão <filipefalcaosantos@gmail.com>
2015-05-09 16:58:36 -0300

    Gourmetize!

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function randomize() {    
+function gourmetize() {    
     var arrayGourmertiza1 = ["Paleta de ", "Costeleta de ", "Risotto de ", "Chutney de ", "Fino Corte de ", "Patê de ", "Carpaccio de ", "Filets de ", "Medalhão de ", "Petit Gateau de ", "Creme de ", "Ravióli de ", "Suflê de "];
     var arrayGourmertiza2 = [" banhado(a) a", " grelhado(a) com", " gratinado(a) em", " marinado(a) em", " salpicado(a) com", " flambado(a) em"];
     var arrayGourmertiza3 = [" trufas negras", " frutas frescas", " molho rosette", "molho branco", " molho de laranja", " molho de manga", "  redução de azeite basamico", " queijo brie" ];
@@ -9,4 +9,5 @@ function randomize() {
     var rand3 = arrayGourmertiza3[Math.floor(Math.random() * arrayGourmertiza3.length)];    
     
     console.log(rand1 + element + rand2 + rand3);
+	alert(rand1 + element + rand2 + rand3);
 }
